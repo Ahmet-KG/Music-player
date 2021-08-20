e1c8400bade17a11aa29eeb89b6c4abc3e7e3d9b
= <bio_ahmet@mail.ru>
2021-08-20 19:02:19 +0600

    funcions for  next and prev buttons

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,22 @@
           {{ current.title }} - <span>{{ current.artist }}</span>
         </h2>
         <div class="controls">
-          <button class="prev">Prev</button>
+          <button class="prev" @click="prev">Prev</button>
           <button class="play" v-if="!isPlaying" @click="play">Play</button>
           <button class="pause" v-else @click="pause">Pause</button>
-          <button class="next">Next</button>
+          <button class="next" @click="next">Next</button>
         </div>
+      </section>
+      <section class="playlist">
+        <h3>The Playlist</h3>
+        <button
+          v-for="song in songs"
+          :key="song.src"
+          @click="play(song)"
+          :class="(song.src == current.src) ? 'song playing' : 'song'"
+        >
+          {{ song.title }} - {{ song.artist }}
+        </button>
       </section>
     </main>
   </div>
@@ -55,6 +66,22 @@ export default {
       this.player.pause();
       this.isPlaying = false;
     },
+    next() {
+      this.index++;
+      if (this.index > this.songs.length - 1) {
+        this.index = 0;
+      }
+      this.current = this.songs[this.index];
+      this.play(this.current);
+    },
+    prev() {
+      this.index--;
+      if (this.index < 0) {
+        this.index = this.songs.length - 1;
+      }
+      this.current = this.songs[this.index];
+      this.play(this.current);
+    }
   },
   created() {
     this.current = this.songs[this.index];
